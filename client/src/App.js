02e6af300467b586090d9e09437fff713c5f42a5
jalvaradoWD <jalvarado1337@lavabit.com>
2020-11-12 17:46:41 -0600

    Created File and Route for the RecipeForm component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
+import RecipeForm from "./components/RecipeForm";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/createRecipe" component={RecipeForm} />
       </Switch>
     </Router>
   );
---
+++ client/src/components/RecipeForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecipeForm = () => {
+  return (
+    <form>
+      <h2>Recipe Form</h2>
+    </form>
+  );
+};
+
+export default RecipeForm;
