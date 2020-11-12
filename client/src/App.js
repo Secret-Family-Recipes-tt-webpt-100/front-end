c433ec51cf94b046f2a04116a9d4ede51c0d44a3
jalvaradoWD <jalvarado1337@lavabit.com>
2020-11-11 20:27:26 -0600

    Setting the React Router Links for the Navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 
+import NavBar from "./components/Navbar";
+
 const App = () => {
   return (
     <Router>
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
