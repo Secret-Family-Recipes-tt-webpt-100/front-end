76f5ed397bea9167387e541114711aa6faad9963
jalvaradoWD <jalvarado1337@lavabit.com>
2020-11-12 19:55:33 -0600

    Added Bootstrap to the project.
    - Bootstrap is aiding in the functionality to the Navbar.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import {
+  Collapse,
+  Nav,
+  Navbar as BsNavbar,
+  NavbarToggler,
+  NavItem,
+  NavLink as BsNavlink,
+} from "reactstrap";
+
 const Navbar = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/signin">Sign In</Link>
-      <Link to="/signup">Sign Up</Link>
-    </nav>
+    <BsNavbar color="light" light expand="md">
+      <NavbarToggler onClick={toggleNavbar} />
+      <Collapse isOpen={!collapsed} navbar>
+        <Nav navbar>
+          <NavItem>
+            <BsNavlink>
+              <Link to="/">Home</Link>
+            </BsNavlink>
+          </NavItem>
+          <NavItem>
+            <BsNavlink>
+              <Link to="/signin">Sign In</Link>
+            </BsNavlink>
+          </NavItem>
+          <NavItem>
+            <BsNavlink>
+              <Link to="/signup">Sign Up</Link>
+            </BsNavlink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </BsNavbar>
   );
 };
 
