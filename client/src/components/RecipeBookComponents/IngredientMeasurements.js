d9bfb74e8621f8320b4426bd5c9232af36c5395f
jalvaradoWD <jalvarado1337@lavabit.com>
2020-11-12 23:08:17 -0600

    Added the IngredientMeaurements component. - A components that handles the state for the ingredients list in the RecipeForm Component.
    Added the event handlers for the RecipeForm's category state.
    - In a later commit, the category state should have it's own component.
    
    A new file called RecipeFormUtils.js has been created
    - This file export function that might be reused in other components.
    
    * Slight adjustments to the styling of the forms.

---
+++ client/src/components/RecipeBookComponents/IngredientMeasurements.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import { addIngredientUtil } from "../../utils/RecipeFormUtils";
+
+const IngredientMeasurements = (props) => {
+  const { formData, setFormData } = props;
+
+  const [amount, setAmount] = useState(0);
+  const [measurement, setMeasurement] = useState("");
+  const [item, setItem] = useState("");
+
+  const [ingredientString, setIngredientString] = useState();
+
+  const onChange = (e) => (setState) => {
+    e.preventDefault();
+    const { value } = e.target;
+
+    setState(value);
+  };
+
+  useEffect(() => {
+    setIngredientString(
+      amount && measurement && item
+        ? `${amount}${measurement} of ${item}`
+        : null
+    );
+  }, [amount, measurement, item]);
+
+  return (
+    <div>
+      <h3>Ingredients</h3>
+      <label htmlFor="ingredientAmount">
+        Amount
+        <input
+          onChange={(e) => onChange(e)(setAmount)}
+          type="number"
+          id="ingredientAmount"
+          value={amount}
+        />
+      </label>
+      <label htmlFor="ingredientMeasurement">
+        Measurement
+        <select
+          onChange={(e) => onChange(e)(setMeasurement)}
+          name="ingredientMeasurement"
+          id="ingredientMeasurement"
+          value={measurement}
+        >
+          <option value="c">Cup</option>
+          <option value="g">gram</option>
+          <option value="kg">Kilogram</option>
+          <option value="L">Liter</option>
+          <option value="lb">Pound</option>
+          <option value="mL">Mililiter</option>
+          <option value="oz">Ounce</option>
+          <option value="pt">Pint</option>
+          <option value="tsp">Teaspoon</option>
+          <option value="Tbsp">Tablespoon</option>
+        </select>
+      </label>
+      <label htmlFor="ingredientItem">
+        Item
+        <input
+          onChange={(e) => onChange(e)(setItem)}
+          type="text"
+          id="ingredientItem"
+          value={item}
+        />
+      </label>
+      <button
+        onClick={(e) =>
+          addIngredientUtil(e)({
+            formData,
+            setFormData,
+            ingredientString,
+            amount,
+            measurement,
+            item,
+            setIngredientString,
+            setAmount,
+            setMeasurement,
+            setItem,
+          })
+        }
+      >
+        Add Ingredient
+      </button>
+    </div>
+  );
+};
+
+export default IngredientMeasurements;
--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 
 import { onChange as customOnChange } from "../utils/FormUtils";
+import FormStyles from "../styles/Form.styles";
+
+import { addCategoryUtil } from "../utils/RecipeFormUtils";
+import IngredientMeasurements from "./RecipeBookComponents/IngredientMeasurements";
 
 const RecipeForm = () => {
   const [formData, setFormData] = useState({
@@ -12,31 +16,65 @@ const RecipeForm = () => {
     category: [],
   });
 
+  const [categoryInput, setCategoryInput] = useState("");
+
   const onChange = (e) => customOnChange(e)(formData, setFormData);
 
+  const addCategory = (e) =>
+    addCategoryUtil(e)({
+      formData,
+      categoryInput,
+      setFormData,
+      setCategoryInput,
+    });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <FormStyles onSubmit={onSubmit}>
       <h2>Recipe Form</h2>
       <label htmlFor="title">
+        Title
         <input id="title" name="title" type="text" />
       </label>
       <label htmlFor="description">
+        Description
         <textarea name="description" id="description"></textarea>
       </label>
       <label htmlFor="source">
+        Source
         <input id="source" name="source" type="text" />
       </label>
       <label htmlFor="instructions">
-        <textarea name="instructions" name="instructions" type="text" />
+        Instructions
+        <textarea name="instructions" type="text" />
       </label>
       <label htmlFor="ingredients">
-        <input name="ingredients" id="ingredients" type="text" />
+        {/* TODO:
+          Create Redux state for this components so no props drilling occurs
+        */}
+        <IngredientMeasurements formData={formData} setFormData={setFormData} />
       </label>
       <label htmlFor="category">
-        <input name="category" id="category" type="text" />
+        <span>
+          {formData.category.map((category, index) => (
+            <p key={index}>{category}</p>
+          ))}
+        </span>
+        Category
+        <input
+          onChange={(e) => setCategoryInput(e.target.value)}
+          name="category"
+          id="category"
+          type="text"
+          value={categoryInput}
+        />
+        <button onClick={addCategory}>Add Category</button>
       </label>
       <input type="submit" value="Create Recipe" />
-    </form>
+    </FormStyles>
   );
 };
 
