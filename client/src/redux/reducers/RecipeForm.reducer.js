2e40a87fc51eae7a339410c6d62fdb82808e5f65
jalvaradoWD <jalvarado1337@lavabit.com>
2020-11-14 15:18:28 -0600

    Redux is now fully functional. The initial state for the Recipe reducer needs to be made with additonal actions later in the day.

---
+++ client/src/redux/reducers/RecipeForm.reducer.js
@@ -0,0 +1,15 @@
+/**
+ * //TODO
+ * Create the default state for this Reducer
+ */
+
+const initialState = {};
+
+const RecipeFormReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
+
+export default RecipeFormReducer;
---
+++ client/src/redux/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import RecipeForm from "./RecipeForm.reducer";
+
+export default combineReducers({ RecipeForm });
